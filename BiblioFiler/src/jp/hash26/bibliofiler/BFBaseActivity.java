44c62f71e2d5a43381f3fdef7fbd5dec8f7b67e3
hash26 <hash.2826@gmail.com>
2013-06-13 08:06:57 +0900

    second commit.

---
+++ BiblioFiler/src/jp/hash26/bibliofiler/BFBaseActivity.java
@@ -0,0 +1,24 @@
+package jp.hash26.bibliofiler;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BFBaseActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_base);
+	}
+
+	/*
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.base, menu);
+		return true;
+	}
+	*/
+
+}
